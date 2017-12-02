c02328e6e72484b8e71648a92b789d9a5066323f
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2017-12-02 18:17:29 -0200

    Web Push Notifications

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({ origin: true });
+var webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,21 +9,50 @@ var cors = require('cors')({ origin: true });
 var serviceAccount = require("./pwagram-fb-key.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://pwagram-90631.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://pwagram-90631.firebaseio.com"
 });
 
 exports.storePostData = functions.https.onRequest(function (request, response) {
-    cors(request, response, function () {
-        admin.database().ref('posts').push({
-            id: request.body.id,
-            title: request.body.title,
-            location: request.body.location,
-            image: request.body.image
-        }).then(function () {
-            response.status(201).json({ message: 'Data stored', id: request.body.id });
-        }).catch(function (err) {
-            response.status(500).json({ error: err });
+  cors(request, response, function () {
+    admin.database().ref('posts').push({
+      id: request.body.id,
+      title: request.body.title,
+      location: request.body.location,
+      image: request.body.image
+    }).then(function () {
+      webpush.setVapidDetails(
+        'mailto:eltongarbin@gmail.com',
+        'BCuZb0tS92J8RjwA284G5TAnQiEoopIz12cP5AZd0IcIvdfcxOypgdaxWlepR7absd7S1ulaomSkBRFi8IBe9yA',
+        'Zl1kNvrh5Bu0Kh1D7eqpvrdbMNacvFXDDy2k9m788ZY'
+      );
+
+      return admin.database().ref('subscriptions').once('value');
+    }).then(function (subscriptions) {
+      subscriptions.forEach(function (sub) {
+        var pushConfig = {
+          endpoint: sub.val().endpoint,
+          keys: {
+            auth: sub.val().keys.auth,
+            p256dh: sub.val().keys.p256dh
+          }
+        }
+
+        webpush.sendNotification(pushConfig, JSON.stringify({
+          title: 'New Post',
+          content: 'New Post added!',
+          openUrl: '/help'
+        })).catch(function (err) {
+          console.log(err);
         });
+      });
+
+      response.status(201).json({
+        message: 'Data stored',
+        id: request.body.id
+      });
+    }).catch(function (err) {
+      response.status(500).json({ error: err });
     });
+  });
 });
--- public/sw.js
+++ public/sw.js
@@ -225,7 +225,7 @@ self.addEventListener('sync', function (event) {
               console.log('Sent data', res);
               if (res.ok) {
                 res.json()
-                  .then(function(resData){
+                  .then(function (resData) {
                     deleteItemFromData('sync-posts', resData.id);
                   });
               }
@@ -236,4 +236,65 @@ self.addEventListener('sync', function (event) {
         })
     );
   }
+});
+
+self.addEventListener('notificationclick', function (event) {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+    notification.close();
+  } else {
+    console.log(action);
+    event.waitUntil(
+      clients.matchAll()
+        .then(function (clis) {
+          var client = clis.find(function (c) {
+            return c.visibilityState === 'visible';
+          });
+
+          if (client !== undefined) {
+            client.navigate(notification.data.url);
+            client.focus();
+          } else {
+            client.openWindow(notification.data.url);
+          }
+          notification.clone();
+        })
+    );
+  }
+});
+
+self.addEventListener('notificationclose', function (event) {
+  console.log('Notification was closed', event);
+});
+
+self.addEventListener('push', function (event) {
+  console.log('Push Notification received', event);
+
+  var data = {
+    title: 'New!',
+    content: 'Something new happened!',
+    openUrl: '/'
+  };
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  var options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+    data: {
+      url: data.openUrl
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 });
